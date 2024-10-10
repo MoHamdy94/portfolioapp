26375f3844a08b069f9523c0604dc187b391f2b6
MoHamdy994 <mohamedhamdy994@gmail.com>
2024-10-10 19:22:58 +0300

    data added to app.js to be readed in projects and projectdetails

--- src/component/Projectdetails.js
+++ src/component/Projectdetails.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-function ProjectDetails() {
+function ProjectDetails({ projects }) {
   const { id } = useParams(); 
+  const project = projects.find(proj => proj.id === id);
 
-  const project = {
-    id: id,
-    title: `Project ${id}`,
-    description: `This is the detailed description of Project ${id}. It contains all the relevant information about the project, including technologies used and challenges faced.`,
-    image: `https://via.placeholder.com/600x400?text=Project+${id}`,
-  };
+  // const project = {
+  //   id: id,
+  //   title: `Project ${id}`,
+  //   description: `This is the detailed description of Project ${id}. It contains all the relevant information about the project, including technologies used and challenges faced.`,
+  //   image: `https://via.placeholder.com/600x400?text=Project+${id}`,
+  // };
 
   return (
     <div className="container project-details mt-5">
     <div className="row">
       <div className="col-md-6">
-        <img src={project.image} alt={project.title} className="img-fluid" />
+        <img src={project.img} alt={project.title} className="img-fluid" />
       </div>
       <div className="col-md-6">
         <h1>{project.title}</h1>
